81d22150f2de1aa8956989fe9824a9dc6dc29c68
Moritz Gobbert <gobbert@uni-trier.de>
2024-06-05 15:54:09 +0200

    more tests

--- mgpy/mgstr.py
+++ mgpy/mgstr.py
@@ -1,29 +1,49 @@
 """A personal module containing methods I use once in a while."""
+
 from enum import StrEnum
+
 
 class Loglevel(StrEnum):
     """A string enum defining importance levels for usage in the 'log_print' method"""
-    INFO = 'Information'
-    WARNING = 'Warning'
-    ERROR = 'ERROR'
+
+    INFO = "Information"
+    WARNING = "Warning"
+    ERROR = "ERROR"
+
 
 def log_print(s: str, level: Loglevel = Loglevel.INFO):
     """Takes a string and outputs it with an additional prefix indicating importance."""
-    print(f'{level}: {s}')
+    if level not in Loglevel._member_names_:
+        level = Loglevel.INFO
+    print(f"{level}: {s}")
 
-def truncate_string(s: str, length: int, ellipsis: str = '...') -> str:
+
+def truncate_string(s: str, length: int, ellipsis: str = "...") -> str:
     """Takes a string, truncates it to {length - n} (where n is the lenght of the ellipsis) characters and adds an ellipsis (default is '...').
     If the argument {s} is not a string, the method just returns the argument.
     """
     if not isinstance(length, int):
         length = len(s)
     if not isinstance(ellipsis, str):
-        ellipsis = '...'
+        ellipsis = "..."
     if not isinstance(s, str):
-        print(f'{s} is not a string.')
+        print(f"{s} is not a string.")
     elif len(s) > length:
         # if the shortened string including the ellipsis is too long, don't add an ellipsis
         if len(ellipsis) > length - len(ellipsis):
-            ellipsis = ''
-        s = f'{s[:length-len(ellipsis)]}{ellipsis}'
+            ellipsis = ""
+        s = f"{s[:length-len(ellipsis)]}{ellipsis}"
     return s
+
+
+def insert_line_into_string(l, s, pos):
+    """Takes two strings and inserts the first one `l` into the second one `s` as a new line at position `pos`"""
+    split_string = s.splitlines(keepends=True)
+    if "\n" not in l:
+        l += "\n"
+    if pos >= len(split_string):
+        l = "\n" + l
+        if "\n" not in split_string[-1]:
+            l = l.rstrip("\n")
+    split_string.insert(pos, l)
+    return "".join(split_string)
--- tests/test_mgstr.py
+++ tests/test_mgstr.py
@@ -1,10 +1,60 @@
+import io
+import sys
+
 from mgpy import mgstr
 
+def test_log_print():
+    hello_world = "Hello, world!"
+
+    capturedOutput = io.StringIO()
+    sys.stdout = capturedOutput
+    mgstr.log_print(hello_world)
+    sys.stdout = sys.__stdout__
+
+    assert f"Information: {hello_world}\n" == capturedOutput.getvalue()
+
+
+def test_log_print_with_loglevel_error():
+    hello_world = "Hello, world!"
+
+    capturedOutput = io.StringIO()
+    sys.stdout = capturedOutput
+    mgstr.log_print(hello_world, mgstr.Loglevel.ERROR)
+    sys.stdout = sys.__stdout__
+
+    assert f"ERROR: {hello_world}\n" == capturedOutput.getvalue()
+
+
+def test_log_print_with_invalid_loglevel():
+    hello_world = "Hello, world!"
+
+    capturedOutput = io.StringIO()
+    sys.stdout = capturedOutput
+    mgstr.log_print(hello_world, [])
+    sys.stdout = sys.__stdout__
+
+    assert f"Information: {hello_world}\n" == capturedOutput.getvalue()
+
+
 def test_truncate_string():
-    assert 'long string...' == mgstr.truncate_string('long string is very long', 11 + 3)
+    long_string = "long string is very long"
+
+    truncated_string = mgstr.truncate_string(long_string, 11 + 3)
+
+    assert "long string..." == truncated_string
+
 
 def test_truncate_string_with_ellipsis():
-    assert 'long string…' == mgstr.truncate_string('long string is very long', 11 + 1, '…')
+    long_string = "long string is very long"
+
+    truncated_string = mgstr.truncate_string(long_string, 11 + 1, "…")
+
+    assert "long string…" == truncated_string
+
 
 def test_truncate_string_with_too_short_length():
-    assert 'sh' == mgstr.truncate_string('short string', 2)
+    short_string = "short string"
+
+    truncated_string = mgstr.truncate_string(short_string, 2)
+
+    assert "sh" == truncated_string
